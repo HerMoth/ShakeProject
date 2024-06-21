02ecc64f193d0662880120551b8bf1f271e9d7f7
Yessenia Nicacio-Rosales <107132405+HerMoth@users.noreply.github.com>
2024-06-21 11:08:43 -0500

    Update powerOff.sh

---
+++ powerOff.sh
@@ -0,0 +1,11 @@
+import RPI.GPIO as GPIO
+from time import sleep 
+dtoverlay = gpio-shutdown
+ledpin = 4
+pushpin = 17
+GPIO.setup($SYS_IS_RS
+echo "Power off?"
+GPIO.setup(pushpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4,GPIO.OUT, Initial = GPIO.low)
+sudo poweroff
+fi
